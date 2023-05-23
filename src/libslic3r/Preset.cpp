fec8aa6c7426be8e6aed21e9a73a5f16f9145e20
Morton Jonuschat <mjonuschat@gmail.com>
2023-05-22 21:07:45 -0700

    [FEATURE] Configurable bridge density
    This is a simplified version of the SuperSlicer "Bridge lines density" feature.
    Where SuperSlicer allows to set upper and lower bounds and adjust spacing
    dynamically in that range this version only allow to set a single value that
    will be used.
    
    This work is based on the implementation in OrcaSlicer by Softfever:
    * https://github.com/SoftFever/OrcaSlicer/commit/fe9a2715e6d9a428bd011a550ffe6b695c96cbc0
    * https://github.com/SoftFever/OrcaSlicer/commit/fe6ce5e28a4755c7d0aff53adde997400db07399
    * https://github.com/SoftFever/OrcaSlicer/commit/ab5e7f7de8e5b54707607c25d267a2dc4fb195c6

--- src/libslic3r/Preset.cpp
+++ src/libslic3r/Preset.cpp
@@ -479,6 +479,8 @@ static std::vector<std::string> s_Preset_print_options {
     "min_width_top_surface",
 
     "solid_fill_pattern",
+
+    "bridge_density",
 };
 
 static std::vector<std::string> s_Preset_filament_options {
